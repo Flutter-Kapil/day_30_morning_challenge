686e67d0ea2ca8538e74189f42c9a4d8a16ed2f5
kapil jhajhria <jhajhria44@gmail.com>
2019-11-14 10:10:10 +0530

    complete,
    with long logic

--- test/test.dart
+++ test/test.dart
@@ -1,24 +1,18 @@
 import 'package:test/test.dart';
-import 'main.dart';
+import '../bin/main.dart';
 
 void main() {
   test("test case 1", () {
     expect(
         matryoshka([
           [2, 3, 9, 5],
-        ], [
-          10,
-          2,
-          1
+          [10, 2, 1]
         ]),
         true);
     expect(
         matryoshka([
           [2, 3, 9, 5],
-        ], [
-          2,
-          2,
-          9
+          [2, 2, 9]
         ]),
         false);
   });
