da7ec405e46beb40a9ff4431eb4c8c38a723ad1a
kapil jhajhria <jhajhria44@gmail.com>
2019-11-14 10:57:18 +0530

    one edge case failing

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 // Challenge
 // Matryoshka dolls are traditionally wooden dolls that can be nested by fitting
 // smaller dolls into larger ones. Suppose arrays can be nested similarly,
@@ -13,37 +15,23 @@
 
 /// Create a function that returns true if every single sub-array inside an array
 /// can be nested Matroyshka style, and false otherwise.
-bool matryoshka(List<List<int>> numbersList) {
-  for (var listItem in numbersList) {
-    if (!canListBeNestedInAnyOtherList(listItem, numbersList)) {
-      return false;
-    }
-  }
-  return true;
-}
+bool matryoshka(List<List<int>> numbersList) => !numbersList
+    .any((subList) => !canListBeNestedInAnyOtherList(subList, numbersList));
+//  numbersList.forEach((subList) => subList.sort());
+//  List listOfMin = [];
+//  List listOfMax = [];
+//  numbersList.forEach((subList) {
+//    listOfMin.add(subList.first);
+//    listOfMax.add(subList.last);
+//  });
+//  print(listOfMin);
+//  print(listOfMax);
+compare2Lists4Nesting(List<int> a, List<int> b) =>
+    (a.reduce(min) > b.reduce(min) && a.reduce(max) < b.reduce(max)) ||
+    (b.reduce(min) > a.reduce(min) && b.reduce(max) < a.reduce(max));
 
-compare2Lists4Nesting(List a, List b) {
-  a.sort();
-  b.sort();
-  int aMin = a.first;
-  int aMax = a.last;
-  int bMin = b.first;
-  int bMax = b.last;
-  // min(array A) > min(array B) and  max(array A) < max(array B)
-  return (aMin > bMin && aMax < bMax) || (bMin > aMin && bMax < aMax);
-}
-
-canListBeNestedInAnyOtherList(List<int> a, List<List<int>> numbersList) {
-  a.sort();
-  int aMin = a.first;
-  int aMax = a.last;
-  for (var x in numbersList) {
-    if (compare2Lists4Nesting(a, x)) {
-      return true;
-    }
-  }
-  return false;
-}
+canListBeNestedInAnyOtherList(List<int> a, List<List<int>> numbersList) =>
+    numbersList.any((subList) => compare2Lists4Nesting(subList, a));
 
 main() {
   //two lists
@@ -52,39 +40,9 @@ main() {
     [10, 2, 1]
   ]));
   //multiple lists
-  print(matryoshka([
-    [8, 10],
-    [7, 11],
-    [6, 12],
-    [5, 13]
-  ]));
-  //just one list
-  print(matryoshka([
-    [2, 3, 9, 5],
-  ]));
-  //two same lists
-  print(matryoshka([
-    [2, 3, 9, 5],
-    [2, 3, 9, 5],
-  ]));
-  //just one item in a oen of the list
-  print(matryoshka([
-    [
-      2,
-    ],
-    [1, 2, 3],
-  ]));
-  //one item in both lists
-  print(matryoshka([
-    [
-      2,
-    ],
-    [2],
-  ]));
-  print(matryoshka([
-    [
-      2,
-    ],
-    [3],
-  ]));
+//  matryoshka([
+//    [4, 9, 8, 14, 11],
+//    [12, 3, 9, 8],
+//    [9, 2, 6, 8]
+//  ]);
 }
--- test/test.dart
+++ test/test.dart
@@ -15,5 +15,82 @@ void main() {
           [2, 2, 9]
         ]),
         false);
+    expect(
+        matryoshka([
+          [1, 10, 6],
+          [4, 8, 2],
+          [5]
+        ]),
+        true);
+    expect(
+        matryoshka([
+          [1, 10, 6],
+          [4, 8, 2],
+          [5]
+        ]),
+        true);
+    expect(
+        matryoshka([
+          [2],
+          [3]
+        ]),
+        false);
+    expect(
+        matryoshka([
+          [2],
+          [2]
+        ]),
+        false);
+    expect(
+        matryoshka([
+          [2, 1, 3],
+          [2]
+        ]),
+        true);
+    expect(
+        matryoshka([
+          [2, 3, 9],
+          [2, 3, 9]
+        ]),
+        false);
+    expect(
+        matryoshka([
+          [2, 9, 3],
+          [2, 9]
+        ]),
+        false);
+    expect(
+        matryoshka([
+          [2, 9, 3]
+        ]),
+        false);
+    expect(
+        matryoshka([
+          [8, 10],
+          [7, 11],
+          [6, 12],
+          [5, 13]
+        ]),
+        true);
+    expect(
+        matryoshka([
+          [1],
+          [8],
+          [2]
+        ]),
+        false);
+    expect(
+        matryoshka([
+          [4, 8, 2],
+          [8],
+        ]),
+        false);
+    expect(
+        matryoshka([
+          [1, 10, 6],
+          [4, 8, 2],
+          [8],
+        ]),
+        false);
   });
 }
